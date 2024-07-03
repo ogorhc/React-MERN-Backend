da7e83eb2087a77ddf29264c4a139c6544ab939d
Tu nombre <i.hinojosa@ingreen.es>
2024-07-03 15:55:01 +0200

    feat: react in public folder

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./database/config');
 const { jwtValidator } = require('./middlewares/jwt-validator');
 require('dotenv').config();
@@ -16,6 +17,10 @@ app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', [jwtValidator], require('./routes/events'));
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT ?? 4000, () => {
   console.log(`Server corriendo en puerto ${process.env.PORT}`);
